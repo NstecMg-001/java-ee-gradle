c4c13e9af5e67bd82fc1adcdfeb6784daf4745d6
nstecemg_001 <nstecemg_001@fullness.email>
2025-07-02 10:13:14 +0900

    Java Sample　追加

--- src/main/java/com/example/servlet/IndexServlet.java
+++ src/main/java/com/example/servlet/IndexServlet.java
@@ -1,19 +1,22 @@
 package com.example.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet("")
+@WebServlet("/hello")
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+        PrintWriter out = response.getWriter();
+        out.println("Hello main page!");
     }
 } 
